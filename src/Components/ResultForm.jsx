1f256388a72e8dbe03b4d6dc9873540ef21d40f2
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2025-02-01 19:36:25 +0400

    The Form content is now copyable

--- src/Components/ResultForm.jsx
+++ src/Components/ResultForm.jsx
@@ -11,6 +11,15 @@ import SaveIcon from '@mui/icons-material/Save';
 export default function Form() {
     const {form, formHeading} = useContext(MyContext);
 
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText(form);
+            console.log('Content copied to clipboard');
+          } catch (err) {
+            console.error('Failed to copy: ', err);
+          }
+    }
+
     useEffect(() => {
         hljs.highlightAll();
       }, []);
@@ -26,7 +35,7 @@ export default function Form() {
                     </pre>
                 </div>
                 <div className="actions">
-                    <Button variant="contained" sx={{backgroundColor:"rgba(0, 0, 0, 0.71)"}} startIcon={<ContentCopyIcon />}>Copy</Button>
+                    <Button variant="contained" onClick={handleCopy} sx={{backgroundColor:"rgba(0, 0, 0, 0.71)"}} startIcon={<ContentCopyIcon />}>Copy</Button>
                     <Button variant="contained" sx={{backgroundColor:"rgba(0, 0, 0, 0.71)"}} startIcon={<SaveIcon />}>Save</Button>
                 </div>
             </div>
