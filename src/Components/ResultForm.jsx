cca3c95efab0fc18762bc2ea4c6f6939b99b3c21
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2025-02-06 12:26:38 +0400

    copying message added

--- src/Components/ResultForm.jsx
+++ src/Components/ResultForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
 import { useContext } from "react";
@@ -7,37 +7,51 @@ import "../css/pre.css";
 import Button from '@mui/material/Button';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import SaveIcon from '@mui/icons-material/Save';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Form() {
     const {form, formHeading} = useContext(MyContext);
+    const [open, setOpen] = useState(false);
 
     const handleCopy = async () => {
+        setOpen(true);
         try {
             await navigator.clipboard.writeText(form);
-            console.log('Content copied to clipboard');
-          } catch (err) {
-            console.error('Failed to copy: ', err);
-          }
+          } catch (err) {}
     }
 
     useEffect(() => {
         hljs.highlightAll();
-      }, []);
+    }, []);
 
-        return <>
-            <div className="formContainer">
-                <div>
-                    <h2>{formHeading.title}</h2><br />
-                    <pre>
-                        <code className="language-html">
-                            {form}
-                        </code>
-                    </pre>
-                </div>
-                <div className="actions">
-                    <Button variant="contained" onClick={handleCopy} sx={{backgroundColor:"rgba(0, 0, 0, 0.71)"}} startIcon={<ContentCopyIcon />}>Copy</Button>
-                    <Button variant="contained" sx={{backgroundColor:"rgba(0, 0, 0, 0.71)"}} startIcon={<SaveIcon />}>Save</Button>
-                </div>
+    const handleClose = (e, r) => {
+        if (e === "clickaway") return;
+
+        setOpen(false);
+    }
+
+    const action = <React.Fragment >
+        <IconButton size="small" color="inherit" onClick={handleClose}>
+        <CloseIcon fontSize="small" /></IconButton></React.Fragment>
+
+    return <>
+        <div className="formContainer">
+            <div>
+                <h2>{formHeading.title}</h2><br />
+                <pre>
+                    <code className="language-html">
+                        {form}
+                    </code>
+                </pre>
             </div>
-        </>
+            <div className="actions">
+                <Button variant="contained" onClick={handleCopy} sx={{backgroundColor:"rgba(0, 0, 0, 0.71)"}} startIcon={<ContentCopyIcon />}>Copy</Button>
+                <Button variant="contained" sx={{backgroundColor:"rgba(0, 0, 0, 0.71)"}} startIcon={<SaveIcon />}>Save</Button>
+            </div>
+        </div>
+
+        <Snackbar open={open} autoHideDuration={3000} sx={{width:"200px"}} onClose={handleClose} message="Content copied" action={action}/>
+    </>
     };
